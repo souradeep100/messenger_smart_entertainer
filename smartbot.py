43129c375eb4f14dd6b3d7dfe06fe69d05c4799a
souradeep100 <souradeep.bond.007@gmail.com>
2017-10-25 00:04:14 +0530

    1st commit
    smartbot.py added

---
+++ smartbot.py
@@ -0,0 +1,38 @@
+#this is new file for the functions of chatbot
+from subprocess import call
+class SmartBot:
+    def __init__(self):
+        self.request_num = 0
+        self.play_music = False
+        self.play_youtube = False
+        self.download_torrent = False
+        self.download_video_site = False
+        self.stop_music = False
+        self.stop_youtube = False
+        self.decrease_volume = False
+        self.increase_volume = False
+        self.stop_downloads = False
+
+    def handle_request(self,request_num=None,request_string=None):
+        if (request_string is not None and request_string.lower() is "hi" or 
+                request_string.lower() is "smarty" or
+                request_string.lower() is "hello"):
+            response = ("Hello..what you want"
+                        "1.play music"
+                        "2.play something on youtube"
+                        "3.download using torrent:P"
+                        "4.download a video from someplace!!!"
+                        "5.stop music"
+                        "6.stop youtube"
+                        "7.decrease volume"
+                        "8.increase volume"
+                        "9.stop downloads")
+        elif (request_num is not None and
+              request_num is 1):
+            self.play_music = True
+            
+            response = "Oops , please greet me nicely :)"
+
+        
+    
+     
